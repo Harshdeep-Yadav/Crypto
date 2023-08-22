c528b46d705ed91b8a598617168560c9b7a13b44
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-08-22 11:02:26 +0530

    Finishing...

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+import React from "react";
+import millify from "millify";
+import { Collapse, Row, Col, Typography, Avatar } from "antd";
+import HTMLReactParser from "html-react-parser";
 
-import { useGetCryptoHistoryQuery } from '../services/cryptoApi';
-import Loader from './Loader';
+import { useGetCryptoHistoryQuery } from "../services/cryptoApi";
+import Loader from "./Loader";
 
 const { Text } = Typography;
 const { Panel } = Collapse;
@@ -17,13 +17,17 @@ const Exchanges = () => {
 
   return (
     <>
-      <Row>
+      <Row  style={{
+            fontSize: '8rem',
+            fontWeight: 'bold',
+           
+          }}>
         <Col span={6}>Exchanges</Col>
         <Col span={6}>24h Trade Volume</Col>
         <Col span={6}>Markets</Col>
         <Col span={6}>Change</Col>
       </Row>
-      <Row>
+      {/* <Row>
         {exchangesList?.map((exchange) => (
           <Col span={24}>
             <Collapse>
@@ -48,7 +52,63 @@ const Exchanges = () => {
             </Collapse>
           </Col>
         ))}
-      </Row>
+      </Row> */}
+
+<div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)', // Gradient background
+      }}
+    >
+      <div
+        style={{
+          textAlign: 'center',
+          padding: '2rem',
+          background: 'white',
+          borderRadius: '8px',
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            marginBottom: '1rem',
+            color: '#333',
+          }}
+        >
+          Rapid API Subscription Required 
+          <br/>
+        </Text>
+        <Text
+          style={{
+            fontSize: '1.2rem',
+            color: '#555',
+          }}
+        >
+          To access this content, you need a Rapid API subscription. Please subscribe to enjoy the data.
+        </Text>
+        <div style={{ marginTop: '1.5rem' }}>
+          <button
+            style={{
+              padding: '0.8rem 2rem',
+              fontSize: '1rem',
+              fontWeight: 'bold',
+              color: 'white',
+              background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
+              border: 'none',
+              borderRadius: '4px',
+              cursor: 'pointer',
+            }}
+          >
+            Subscribe Now
+          </button>
+        </div>
+      </div>
+    </div>
     </>
   );
 };
